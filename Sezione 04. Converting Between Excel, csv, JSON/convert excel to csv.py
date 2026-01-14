59ce3d01e2b886c6e9e904ec759fdf8f9ef98147
Domenico <domenicosanto.dev@gmail.com>
2026-01-14 09:39:36 +0100

    conversione file excel to csv

---
+++ Sezione 04. Converting Between Excel, csv, JSON/convert excel to csv.py
@@ -0,0 +1,15 @@
+# libreria suggerita `Pandas` e per la gestione dei file excel `openpyxl`
+# dalla documentazioni di Pandas suggeriva appunto l'installazione includendo la dipendenza per la lettura di file Excel:
+# pip install "pandas[excel]"
+
+# Creare un programma che converte un file excel in un file CSV.
+# leggi il file europe.xlsx e salvi convertendo in europe.csv
+import pandas as pd
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+file_excel = BASE_DIR / "europe.xlsx"
+file_csv = BASE_DIR / "europe.csv"
+
+df = pd.read_excel(file_excel)
+df.to_csv(file_csv, index=False, sep=",")
