76a4219db65c1ac040617a33d55e7d80519e3d74
Domenico <domenicosanto.dev@gmail.com>
2026-01-14 09:42:42 +0100

    aggiunti commenti

--- Sezione 04. Converting Between Excel, csv, JSON/convert excel to csv.py
+++ Sezione 04. Converting Between Excel, csv, JSON/convert excel to csv.py
@@ -3,7 +3,7 @@
 # pip install "pandas[excel]"
 
 # Creare un programma che converte un file excel in un file CSV.
-# leggi il file europe.xlsx e salvi convertendo in europe.csv
+# leggi il file europe.xlsx e salvi convertendo in europe.csv nella stessa directory dello script.
 import pandas as pd
 from pathlib import Path
 
@@ -11,5 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent
 file_excel = BASE_DIR / "europe.xlsx"
 file_csv = BASE_DIR / "europe.csv"
 
+# legge il file excel
 df = pd.read_excel(file_excel)
+
+# converte il df in csv file
 df.to_csv(file_csv, index=False, sep=",")
