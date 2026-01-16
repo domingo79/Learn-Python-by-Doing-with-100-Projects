e1d37953671fd8b660cc482125e3e6f1c5dda7ed
Domenico <domenicosanto.dev@gmail.com>
2026-01-16 16:22:43 +0100

    progetto aggiungi img a dropbox (tutorial)

--- Sezione 09. Using the Dropbox API/uploading a file to dropbox with python(tutorial).py
+++ Sezione 09. Using the Dropbox API/uploading a file to dropbox with python(tutorial).py
@@ -3,9 +3,13 @@ import dropbox
 from pathlib import Path
 import dotenv
 
-ROOT = Path(__file__).resolve().parent.parent
+# percorsi cartella root e file
+CARTELLA = Path(__file__).resolve().parent
+ROOT = CARTELLA.parent
 PATH_ENV = ROOT / ".env"
+chicago_img = CARTELLA / "chicago.jpg"
 
+# cerca token
 dotenv.load_dotenv(dotenv_path=PATH_ENV)
 token = os.getenv("DROPBOX_TOKEN")
 if token:
@@ -13,3 +17,15 @@ if token:
     print(f"✅ Token caricato correttamente: {token[:5]}...")
 else:
     print("❌ ERRORE: Non sono riuscito a trovare la chiave 'DROPBOX_TOKEN'.")
+
+# accesso a dropbox
+dbx = dropbox.Dropbox(token)
+file_name = 'chicago.jpg'
+
+# accesso all'immagine
+with open(chicago_img, "rb") as f:
+    content = f.read()
+    # uploaded img
+    dbx.files_upload(content, f'/{file_name}',
+                     mode=dropbox.files.WriteMode('overwrite'))
+    print(f"File {file_name} uploaded successfully!")
