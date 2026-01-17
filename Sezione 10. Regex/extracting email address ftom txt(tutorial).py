6104cc2985f380990c74fb971826d5b529999cef
dsantodev <dsanto.consult@gmail.com>
2026-01-17 21:01:29 +0100

    adottato l acrtella risorse

--- Sezione 10. Regex/extracting email address ftom txt(tutorial).py
+++ Sezione 10. Regex/extracting email address ftom txt(tutorial).py
@@ -1,21 +1,21 @@
 import os
 import re
 
-# percorso alla cartella dove si trova lo script
+# Percorso alla cartella dove si trova lo script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# pecorso alla sotto cartella documents
-documents_dir = os.path.join(script_dir, "documents")
+# Percorso locale alla sezione
+resources_dir = os.path.join(script_dir, "resources")
 
-# otteniamo tutti i file allinterno di documents
-filenames = os.listdir(documents_dir)
+# otteniamo tutti i file allinterno di resources
+filenames = os.listdir(resources_dir)
 
 pattern_email = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}"
 all_email = []
 
 for filename in filenames:
     # ricostruiamo l'intero percorso del file per darlo in pasto ad open()
-    filepath = os.path.join(documents_dir, filename)
+    filepath = os.path.join(resources_dir, filename)
     with open(filepath, "r") as f:
         content = f.read()
 
