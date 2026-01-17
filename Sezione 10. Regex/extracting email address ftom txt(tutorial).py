4c57d3dcd312f9e041aff838648173534addd0f4
dsantodev <dsanto.consult@gmail.com>
2026-01-17 17:00:04 +0100

    aggiunta nota alla guida os applicao il tutto al tutorial

--- Sezione 10. Regex/extracting email address ftom txt(tutorial).py
+++ Sezione 10. Regex/extracting email address ftom txt(tutorial).py
@@ -1,18 +1,21 @@
 import os
 import re
 
-# percorso relativo
-directory = 'Sezione 10. Regex/documents'
+# percorso alla cartella dove si trova lo script
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# otteniamo tutti i file allinterno di directory
-filenames = os.listdir(directory)
+# pecorso alla sotto cartella documents
+documents_dir = os.path.join(script_dir, "documents")
+
+# otteniamo tutti i file allinterno di documents
+filenames = os.listdir(documents_dir)
 
 pattern_email = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}"
 all_email = []
 
 for filename in filenames:
     # ricostruiamo l'intero percorso del file per darlo in pasto ad open()
-    filepath = os.path.join(directory, filename)
+    filepath = os.path.join(documents_dir, filename)
     with open(filepath, "r") as f:
         content = f.read()
 
