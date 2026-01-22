2fae89bf6768558395617f0b9275474f9a558df9
dsantodev <dsanto.consult@gmail.com>
2026-01-22 16:56:10 +0100

    sistemate le directory

--- Sezione 12. Merge Excel Files with Python/merge excel file(Tutorial).py
+++ Sezione 12. Merge Excel Files with Python/merge excel file(Tutorial).py
@@ -1,1 +1,17 @@
+"""
+Creeremo un programma che andrà automaticamente nella entry e troverà tutti i file Excel e ne unisce il contenuto.
+Quindi crea un nuovo file Excel in cui sarà contenuto tutto il contenuto unito.
+"""
+import os
 import pandas as pd
+
+# directory cartella progetto ..\Sezione 12. Merge Excel Files with Python
+FOLDER = os.path.dirname(os.path.abspath(__file__)) 
+# directori alla sotto cartella ..\Sezione 12...\resources
+SUBFOLDER = os.path.join(FOLDER, 'resources') 
+# uso una listcomprehension per escludere il file .gitseek 
+# e ottenere la lista dei file ['email_addresses1.xlsx', 'email_addresses2.xlsx']
+filenames = [f for f in os.listdir(SUBFOLDER) if not f.startswith(".")]
+print("Lista dei filenames:",filenames)
+filepaths = [os.path.join(SUBFOLDER, filename) for filename in filenames]
+print("Percorsi dei filename detti filepaths:",filepaths)
