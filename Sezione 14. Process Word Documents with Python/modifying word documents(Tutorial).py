44c46ef0883d22c31d7cf9e350fd6b35898a8027
dsantodev <dsanto.consult@gmail.com>
2026-01-23 12:08:37 +0100

    modifica dei paragrafi in word con python

---
+++ Sezione 14. Process Word Documents with Python/modifying word documents(Tutorial).py
@@ -0,0 +1,25 @@
+from docx import Document
+import os
+"""
+document = Document()
+document.add_paragraph("It was a dark and stormy night.")
+document.save("dark-and-stormy.docx")
+document = Document("dark-and-stormy.docx")
+document.paragraphs[0].text
+"""
+FOLDER = os.path.dirname(os.path.abspath(__file__))
+RESOURCES = os.path.join(FOLDER, 'resources')
+OUTPUT = os.path.join(FOLDER, 'output')
+# percorso dei singoli file
+doc_path_1, doc_path_2 = [os.path.join(
+    RESOURCES, f) for f in os.listdir(RESOURCES)]
+
+# selezioniamo i due documenti
+doc_1 = Document(doc_path_1)
+doc_2 = Document(doc_path_2)
+
+parag = doc_2.paragraphs[0]
+
+paragraphs = doc_1.paragraphs
+paragraphs[1]._element.addnext(parag._element)
+doc_1.save(os.path.join(OUTPUT, 'panda.docx'))
